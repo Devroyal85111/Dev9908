175ac41f46384b3f142a7614556dcf131960cbd8
your name <you@example.com>
2025-04-25 15:35:36 +0530

    string

---
+++ string_1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char name[100]={'D','E','V',' ','P','R','A','J','A','P','A','T'};                                      
+    printf("The String  %s",name);
+
+    return 0;
+}
